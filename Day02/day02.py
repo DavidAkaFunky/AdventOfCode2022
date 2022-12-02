51199068fce77e26b90eb1241b347d9663ab6017
David Belchior <futebol4@hotmail.com>
2022-12-02 11:09:33 +0000

    Add small optimisation to day 2

--- Day02/day02.py
+++ Day02/day02.py
@@ -1,22 +1,19 @@
-def part1(lines):
+def solve(lines, transform = False):
     results = {"A": {"X": 3, "Y": 6, "Z": 0}, "B": {"X": 0, "Y": 3, "Z": 6}, "C": {"Y": 0, "Z": 3, "X": 6}}
     points = {"X": 1, "Y": 2, "Z": 3}
+    transformations = {"A": {"X": "Z", "Y": "X", "Z": "Y"}, "B": {"X": "X", "Y": "Y", "Z": "Z"}, "C": {"X": "Y", "Y": "Z", "Z": "X"}}
     total = 0
     for line in lines:
+        if transform:
+            line[1] = transformations[line[0]][line[1]]
         total += results[line[0]][line[1]] + points[line[1]]
     return total
-
-def part2(lines):
-    transformations = {"A": {"X": "Z", "Y": "X", "Z": "Y"}, "B": {"X": "X", "Y": "Y", "Z": "Z"}, "C": {"X": "Y", "Y": "Z", "Z": "X"}}
-    for line in lines:
-        line[1] = transformations[line[0]][line[1]]
-    return part1(lines)
 
 def main():
     with open("input.txt", "r") as f:
         lines = [line.strip().split() for line in f.readlines()]
-        print(part1(lines)) # Part 1: 14297
-        print(part2(lines)) # Part 2: 10498
+        print(solve(lines)) # Part 1: 14297
+        print(solve(lines, transform = True)) # Part 2: 10498
 
 if __name__ == "__main__":
     main()
